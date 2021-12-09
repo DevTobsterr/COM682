f4e4d8cd7edb8ed3beb8e64d787d9913a2a42c74
Toby Killen <itoby24@gmail.com>
2021-12-09 14:38:25 +0000

    Security Commit

--- app.py
+++ app.py
@@ -3,13 +3,9 @@ from flask.helpers import make_response
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def application_home():
     return render_template("application_home.html")
-
-
-
 
 
 if __name__ == "__main__":
